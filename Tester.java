f204272d4eda483c8d20e35ed8f827277cdeb9d3
kentcollins <kent.r.collins@gmail.com>
2016-10-14 09:11:14 +0100

    instructions added to Tester

--- Tester.java
+++ Tester.java
@@ -2,8 +2,29 @@
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO 
+		// TODO invoke the stripped method of Palindrome on "Madam", "Ma'am", and ""
+		
+		
+		
+		
+		// TODO invoke the String toLowerCase on the returned values
+		
+		
+		
+		
+		
+		// TODO invoke the evaluate method of Palindrome on the returned values
+		
+		
+		
+		
+		
+		// TODO print the original string and whether it is a palindrome
 
+		
+		
+		
+		
 	}
 
 }
