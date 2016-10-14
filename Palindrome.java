ae938ff53fac9a9d8abc1759c0a256f4c458dec6
kentcollins <kent.r.collins@gmail.com>
2016-10-14 09:07:05 +0100

    Initial commit

---
+++ Palindrome.java
@@ -0,0 +1,48 @@
+/***
+ * Collection of class methods for evaluating potential palindromes
+ * 
+ * @author kentc14
+ * @version	14 October, 2016
+ *
+ */
+public class Palindrome {
+
+	/**
+	 * Removes white space and punctuation (non-letters) from a string.
+	 * Method name - stripped
+	 * Input - a string containing potentially letters and non-letters.
+	 * Output - a version of the string with the original letters in sequence and 
+	 * the non-letters removed.
+	 */
+	//Write the method header and your implementation below
+	
+	
+	
+	
+	/**
+	 * Determines whether a character n-letters from the beginning of
+	 * a stripped string matches the corresponding character n-letters from the 
+	 * end of the stripped string.
+	 * Method name - positionMatch
+	 * Input - a string containing no non-letters
+	 * Input - an integer representing the distance to index from the front/back
+	 * Output - whether the two characters match
+	 */
+	//Write the method header and your implementation below
+	
+	
+	
+	
+	
+	/**
+	 * This method determines whether a string consisting solely of lower case 
+	 * letters reads the same from the front as from the back.
+	 * Method name - evaluate
+	 * Input - a string containing only lower case letters
+	 * Output - whether for each character in the string the corresponding 
+	 * character from the back matches.
+	 */
+	//Write the method header and your implementation below
+	
+	
+}
